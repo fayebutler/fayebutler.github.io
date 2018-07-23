a1a36202c82b9e00069477bf3455ac1283d87cff
Faye Butler <faye.alexandra.butler1@gmail.com>
2018-07-23 14:44:15 +0100

    adding favicon

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 
-
-$('.fa-caret-right').on('click', function(e) {
-  console.log(e.target.dataset['id']);
-  var id = e.target.dataset['id'];
-  $('#' + id).toggle();
-  $(e.target).toggleClass('rotate');
-})
-
-
 $('.reload').on('click', function(e) {
   console.log(e.target.dataset['id']);
   var id = e.target.dataset['id'];
